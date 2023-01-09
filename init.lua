aed1d062c7dd945402cc989be1243f7177c88d0a
jgmdev <jgmdev@gmail.com>
2023-01-09 17:31:11 -0400

    manifest adjustments as suggested by Adam

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- mod-version:3
 -- Example Lua plugin that makes use of the native sample plugin.
 local core = require "core"
-local sample = require "libraries.sample"
+local sample = require "libraries.sample.native"
 
 core.log("%s", sample.greet("John"))
